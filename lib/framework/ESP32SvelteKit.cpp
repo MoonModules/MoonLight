911a286e527ee50e4888243c10811486f48576ba
limpkin <limpkin@limpkin.fr>
2025-09-01 22:50:39 +0200

    esp32svelte kit init code added

--- lib/framework/ESP32SvelteKit.cpp
+++ lib/framework/ESP32SvelteKit.cpp
@@ -54,6 +54,9 @@ ESP32SvelteKit::ESP32SvelteKit(PsychicHttpServer *server, unsigned int numberEnd
 #if FT_ENABLED(FT_BATTERY)
                                                                                           _batteryService(&_socket),
 #endif
+#if FT_ENABLED(FT_VIN_MONITOR) || FT_ENABLED(FT_IIN_MONITOR)
+                                                                                          _electricityInputService(&_socket),
+#endif
 #if FT_ENABLED(FT_ANALYTICS)
                                                                                           _analyticsService(&_socket),
 #endif
