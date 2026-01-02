c33dd2dd6603f12c333821d9b93e664e670f3bbe
ewowi <ewowi@icloud.com>
2026-01-02 12:45:40 +0100

    🐰 recommendations

--- src/MoonBase/pal.h
+++ src/MoonBase/pal.h
@@ -1,5 +1,5 @@
 /**
-    @title     MoonLight
+    @title     MoonBase
     @file      pal.h
     @repo      https://github.com/MoonModules/MoonLight, submit changes to this file as PRs
     @Authors   https://github.com/MoonModules/MoonLight/commits/main
@@ -12,19 +12,19 @@
 /**
  Platform Abstraction Layer (PAL)
 
- Goal: 
+ Goal:
  * not depending on Arduino.h
  * use esp-idf functions (instead of arduino.h)
  * in a way to not create a new dependency on esp-idf
- 
+
  Approach
  * Incremental changes: you can still do it the old way but the new way is available as well
- 
+
  Benefits
  * Common way of working, within one repository, Possibly between different repo's
  * No mix of arduino and esp-idf code in different places
  * platform Arduino can be removed in the future
- * Automated testing e.g. on Linux 
+ * Automated testing e.g. on Linux
  **/
 
 #pragma once
@@ -39,9 +39,9 @@ namespace pal {
  * ================================================= */
 
 struct rgb_t {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
 };
 
 /* =================================================
@@ -50,7 +50,7 @@ struct rgb_t {
 
 uint32_t millis();
 uint64_t micros();
-void     delay_ms(uint32_t ms);
+void delay_ms(uint32_t ms);
 
 /* =================================================
  * LED output (hot path)
@@ -64,18 +64,13 @@ void led_set_brightness(uint8_t brightness);
  * ================================================= */
 
 void* malloc(size_t size);
-void  free(void* ptr);
+void free(void* ptr);
 
 /* =================================================
  * Logging
  * ================================================= */
 
-enum class LogLevel : uint8_t {
-    Error,
-    Warn,
-    Info,
-    Debug
-};
+enum class LogLevel : uint8_t { Error, Warn, Info, Debug };
 
 void log(LogLevel level, const char* tag, const char* message);
 
@@ -91,18 +86,19 @@ int cap_led_parallel();
  * ================================================= */
 
 class UdpSocket {
-public:
-    virtual ~UdpSocket() = default;
+ public:
+  virtual ~UdpSocket() = default;
 
-    virtual bool open(uint16_t local_port) = 0;
-    virtual int  send_to(const char* ip,
-                         uint16_t port,
-                         const uint8_t* data,
-                         size_t len) = 0;
-    virtual void close() = 0;
+  virtual bool open(uint16_t local_port) = 0;
+  virtual int send_to(const char* ip, uint16_t port, const uint8_t* data, size_t len) = 0;
+  virtual int recv_from(uint8_t* buffer, size_t max_len,
+                        char* src_ip,       // out
+                        uint16_t* src_port  // out
+                        ) = 0;
+  virtual void close() = 0;
 };
 
 UdpSocket* udp_socket_create();
-void       udp_socket_destroy(UdpSocket* socket);
+void udp_socket_destroy(UdpSocket* socket);
 
-} // namespace pal
+}  // namespace pal
--- src/MoonBase/pal_espidf.cpp
+++ src/MoonBase/pal_espidf.cpp
@@ -1,6 +1,6 @@
 /**
-    @title     MoonLight
-    @file      pal_espidf.h
+    @title     MoonBase
+    @file      pal_espidf.cpp
     @repo      https://github.com/MoonModules/MoonLight, submit changes to this file as PRs
     @Authors   https://github.com/MoonModules/MoonLight/commits/main
     @Doc       https://moonmodules.org/MoonLight/moonlight/overview/
@@ -9,19 +9,16 @@
     @license   For non GPL-v3 usage, commercial licenses must be purchased. Contact us for more information.
 **/
 
-#include "pal.h"
-
-#include <string.h>
-
+#include <esp_heap_caps.h>
+#include <esp_log.h>
+#include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-
-#include <esp_timer.h>
-#include <esp_log.h>
-#include <esp_heap_caps.h>
-
-#include <lwip/sockets.h>
 #include <lwip/inet.h>
+#include <lwip/sockets.h>
+#include <string.h>
+
+#include "pal.h"
 
 namespace pal {
 
@@ -29,20 +26,11 @@ namespace pal {
  * Time
  * ================================================= */
 
-uint32_t millis()
-{
-    return static_cast<uint32_t>(esp_timer_get_time() / 1000ULL);
-}
+uint32_t millis() { return static_cast<uint32_t>(esp_timer_get_time() / 1000ULL); }
 
-uint64_t micros()
-{
-    return static_cast<uint64_t>(esp_timer_get_time());
-}
+uint64_t micros() { return static_cast<uint64_t>(esp_timer_get_time()); }
 
-void delay_ms(uint32_t ms)
-{
-    vTaskDelay(pdMS_TO_TICKS(ms));
-}
+void delay_ms(uint32_t ms) { vTaskDelay(pdMS_TO_TICKS(ms)); }
 
 /* =================================================
  * LED output (ESP-IDF backend placeholder)
@@ -50,136 +38,122 @@ void delay_ms(uint32_t ms)
 
 static uint8_t s_led_brightness = 255;
 
-void led_submit(const rgb_t* buffer, size_t led_count)
-{
-    /* To be implemented with:
-       - RMT
-       - PARLIO
-       - DMA / PPA
-       - or external driver
-    */
-    (void)buffer;
-    (void)led_count;
+void led_submit(const rgb_t* buffer, size_t led_count) {
+  /* To be implemented with:
+     - RMT
+     - PARLIO
+     - DMA / PPA
+     - or external driver
+  */
+  (void)buffer;
+  (void)led_count;
 }
 
-void led_set_brightness(uint8_t brightness)
-{
-    s_led_brightness = brightness;
-}
+void led_set_brightness(uint8_t brightness) { s_led_brightness = brightness; }
 
 /* =================================================
  * Memory
  * ================================================= */
 
-void* malloc(size_t size)
-{
-    return heap_caps_malloc(size, MALLOC_CAP_8BIT);
-}
+void* malloc(size_t size) { return heap_caps_malloc(size, MALLOC_CAP_8BIT); }
 
-void free(void* ptr)
-{
-    heap_caps_free(ptr);
-}
+void free(void* ptr) { heap_caps_free(ptr); }
 
 /* =================================================
  * Logging
  * ================================================= */
 
-void log(LogLevel level, const char* tag, const char* message)
-{
-    esp_log_level_t esp_level = ESP_LOG_INFO;
+void log(LogLevel level, const char* tag, const char* message) {
+  esp_log_level_t esp_level = ESP_LOG_INFO;
 
-    switch (level) {
-        case LogLevel::Error: esp_level = ESP_LOG_ERROR; break;
-        case LogLevel::Warn:  esp_level = ESP_LOG_WARN;  break;
-        case LogLevel::Info:  esp_level = ESP_LOG_INFO;  break;
-        case LogLevel::Debug: esp_level = ESP_LOG_DEBUG; break;
-    }
+  switch (level) {
+  case LogLevel::Error:
+    esp_level = ESP_LOG_ERROR;
+    break;
+  case LogLevel::Warn:
+    esp_level = ESP_LOG_WARN;
+    break;
+  case LogLevel::Info:
+    esp_level = ESP_LOG_INFO;
+    break;
+  case LogLevel::Debug:
+    esp_level = ESP_LOG_DEBUG;
+    break;
+  }
 
-    esp_log_write(esp_level, tag, "%s", message);
+  esp_log_write(esp_level, tag, "%s\n", message);
 }
 
 /* =================================================
  * Capabilities
  * ================================================= */
 
-int cap_led_dma()
-{
-    return 1; /* ESP32 supports DMA */
-}
+int cap_led_dma() { return 1; /* ESP32 supports DMA */ }
 
-int cap_led_parallel()
-{
-    return 1; /* ESP32 generally supports parallel output */
-}
+int cap_led_parallel() { return 1; /* ESP32 generally supports parallel output */ }
 
 /* =================================================
  * UDP socket (ESP-IDF / lwIP)
  * ================================================= */
 
 class EspIdfUdpSocket : public UdpSocket {
-public:
-    EspIdfUdpSocket() : sock_(-1) {}
+ public:
+  EspIdfUdpSocket() : sock_(-1) {}
 
-    bool open(uint16_t local_port) override
-    {
-        sock_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-        if (sock_ < 0) {
-            return false;
-        }
+  ~EspIdfUdpSocket() override { close(); }
 
-        sockaddr_in addr{};
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(local_port);
-        addr.sin_addr.s_addr = INADDR_ANY;
-
-        return bind(sock_,
-                    reinterpret_cast<sockaddr*>(&addr),
-                    sizeof(addr)) == 0;
+  bool open(uint16_t local_port) override {
+    sock_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock_ < 0) {
+      return false;
     }
 
-    int send_to(const char* ip,
-                uint16_t port,
-                const uint8_t* data,
-                size_t len) override
-    {
-        sockaddr_in dest{};
-        dest.sin_family = AF_INET;
-        dest.sin_port = htons(port);
-        inet_aton(ip, &dest.sin_addr);
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(local_port);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-        return sendto(sock_,
-                      data,
-                      len,
-                      0,
-                      reinterpret_cast<sockaddr*>(&dest),
-                      sizeof(dest));
+    if (bind(sock_, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) != 0) {
+      ::close(sock_);
+      sock_ = -1;
+      return false;
     }
+    return true;
+  }
 
-    void close() override
-    {
-        if (sock_ >= 0) {
-            ::close(sock_);
-            sock_ = -1;
-        }
+  int send_to(const char* ip, uint16_t port, const uint8_t* data, size_t len) override {
+    sockaddr_in dest{};
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(port);
+    inet_aton(ip, &dest.sin_addr);
+
+    return sendto(sock_, data, len, 0, reinterpret_cast<sockaddr*>(&dest), sizeof(dest));
+  }
+
+  int recv_from(uint8_t* buffer, size_t max_len,
+                char* src_ip,       // out
+                uint16_t* src_port  // out
+  ) {
+    return -1;  // to do
+  }
+
+  void close() override {
+    if (sock_ >= 0) {
+      ::close(sock_);
+      sock_ = -1;
     }
+  }
 
-private:
-    int sock_;
+ private:
+  int sock_;
 };
 
 /* =================================================
  * Factory
  * ================================================= */
 
-UdpSocket* udp_socket_create()
-{
-    return new EspIdfUdpSocket();
-}
+UdpSocket* udp_socket_create() { return new EspIdfUdpSocket(); }
 
-void udp_socket_destroy(UdpSocket* socket)
-{
-    delete socket;
-}
+void udp_socket_destroy(UdpSocket* socket) { delete socket; }
 
-} // namespace pal
+}  // namespace pal
