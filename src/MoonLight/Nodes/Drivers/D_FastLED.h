589443a25e0f24593894555fdbdbafb63601d2d1
ewowi <ewowi@icloud.com>
2026-02-08 11:54:46 +0100

    FastLED driver controls
    FastLED driver
    - DriverNode subclass (lightsPreset control)
    - add affinity and temperature control
    - onUpdate lightPreset, affinity and temperature
    - Destructor removing channels - WIP

--- src/MoonLight/Nodes/Drivers/D_FastLED.h
+++ src/MoonLight/Nodes/Drivers/D_FastLED.h
@@ -13,15 +13,7 @@
 
 #if FT_MOONLIGHT
 
-// #include "fl/channels/channel.h"
-// #include "fl/channels/config.h"
-// #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
-//   #include "platforms/esp/32/drivers/spi/channel_engine_spi.h"
-// #else
-//   #include "platforms/esp/32/drivers/parlio/channel_engine_parlio.h"
-// #endif
-
-class FastLEDDriver : public Node {
+class FastLEDDriver : public DriverNode {
  public:
   static const char* name() { return "FastLED Driver"; }
   static uint8_t dim() { return _NoD; }
@@ -29,10 +21,155 @@ class FastLEDDriver : public Node {
 
   char version[20] = TOSTRING(FASTLED_VERSION);  // "." TOSTRING(FASTLED_VERSION_MINOR) "." TOSTRING(FASTLED_VERSION_PATCH);
   Char<32> status = "ok";
+  #if CONFIG_IDF_TARGET_ESP32S3
+  uint8_t affinity = 2;  // I2S
+  #elif CONFIG_IDF_TARGET_ESP32P4
+  uint8_t affinity = 4;  // parlio
+  #else
+  uint8_t affinity = 0;  // auto
+  #endif
+  uint8_t temperature = 0;
 
   void setup() override {
+    DriverNode::setup();  // !!
+
+    addControl(affinity, "affinity", "select");
+    addControlValue("Auto");
+    addControlValue("RMT");
+    addControlValue("I2S");
+    addControlValue("SPI");
+  #if CONFIG_IDF_TARGET_ESP32P4
+    addControlValue("Parlio");
+  #endif
+
+    addControl(temperature, "temperature", "select");
+    addControlValue("Uncorrected");
+    addControlValue("Candle");
+    addControlValue("Tungsten100W");
+    addControlValue("Halogen");
+
     addControl(version, "version", "text", 0, 20, true);
     addControl(status, "status", "text", 0, 32, true);
+  }
+
+  fl::EOrder rgbOrder;
+  fl::ChannelOptions options;
+
+  void onUpdate(const Char<20>& oldValue, const JsonObject& control) override {
+    DriverNode::onUpdate(oldValue, control);  // !!
+
+    LightsHeader* header = &layerP.lights.header;
+
+    EXT_LOGD(ML_TAG, "%s: %s ", control["name"].as<const char*>(), control["value"].as<String>().c_str());
+
+    if (control["name"] == "lightPreset") {
+      switch (layerP.lights.header.lightPreset) {
+      case lightPreset_RGB:
+        rgbOrder = RGB;
+        break;
+      case lightPreset_RBG:
+        rgbOrder = RBG;
+        break;
+      case lightPreset_GRB:
+        rgbOrder = GRB;
+        break;
+      case lightPreset_GBR:
+        rgbOrder = GBR;
+        break;
+      case lightPreset_BRG:
+        rgbOrder = BRG;
+        break;
+      case lightPreset_BGR:
+        rgbOrder = BGR;
+        break;
+      case lightPreset_RGBW:
+        rgbOrder = RGB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W3;
+        break;
+      case lightPreset_GRBW:
+        rgbOrder = GRB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W3;
+        break;
+      case lightPreset_WRGB:
+        rgbOrder = RGB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W0;
+        break;
+      case lightPreset_GRB6:
+        rgbOrder = GRB;
+        break;
+      case lightPreset_RGB2040:
+        // RGB2040 uses standard RGB offsets but has special channel remapping
+        // for dual-channel-group architecture (handled in VirtualLayer)
+        rgbOrder = RGB;
+        break;
+      case lightPreset_RGBWYP:
+        rgbOrder = RGB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W3;
+        break;
+      case lightPreset_MHBeeEyes150W15:
+        rgbOrder = RGB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W3;
+        break;
+      case lightPreset_MHBeTopper19x15W32:
+        rgbOrder = RGB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W3;
+        break;
+      case lightPreset_MH19x15W24:
+        rgbOrder = RGB;
+        options.mRgbw.rgbw_mode = kRGBWExactColors;
+        options.mRgbw.w_placement = W3;
+        break;
+      default:
+        rgbOrder = GRB;
+        break;
+      }
+    }
+
+    if (control["name"] == "affinity") {
+      switch (control["value"].as<uint8_t>()) {
+      case 0:
+        options.mAffinity = "";
+        break;  // auto
+      case 1:
+        options.mAffinity = "RMT";
+        break;
+      case 2:
+        options.mAffinity = "I2S";
+        break;
+      case 3:
+        options.mAffinity = "SPI";
+        break;
+      case 4:
+        options.mAffinity = "PARLIO";
+        break;
+      }
+      FastLED.setExclusiveDriver(options.mAffinity.c_str());
+
+      if (control["name"] == "temperature") {
+        switch (control["value"].as<uint8_t>()) {
+        case 0:
+          options.mTemperature = UncorrectedTemperature;
+          break;
+        case 1:
+          options.mTemperature = Candle;
+          break;
+        case 2:
+          options.mTemperature = Tungsten100W;
+          break;
+        case 3:
+          options.mTemperature = Halogen;
+          break;
+        }
+      }
+
+      options.mCorrection = TypicalLEDStrip;  // gamma ...
+    }
   }
 
   uint16_t savedMaxPower = UINT16_MAX;
@@ -103,43 +240,31 @@ class FastLEDDriver : public Node {
       fl::ChipsetTimingConfig timing = fl::makeTimingConfig<fl::TIMING_WS2812_800KHZ>();
       CRGB* leds = (CRGB*)layerP.lights.channelsD;
       uint16_t startLed = 0;
-  #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
-      FastLED.setExclusiveDriver("I2S");
-  #else
-      FastLED.setExclusiveDriver("PARLIO");
-  #endif
 
       for (uint8_t pinIndex = 0; pinIndex < nrOfPins; pinIndex++) {
         EXT_LOGD(ML_TAG, "ledPin p:%d #:%d", pins[pinIndex], layerP.ledsPerPin[pinIndex]);
 
-        fl::ChannelOptions options;
-  #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
-        options.mAffinity = "I2S";
-  #else
-        options.mAffinity = "PARLIO";
-  #endif
-
-        fl::ChannelConfig config(pins[pinIndex], timing, fl::span<CRGB>(&leds[startLed], layerP.ledsPerPin[pinIndex]), GRB, options);
+        fl::ChannelConfig config(pins[pinIndex], timing, fl::span<CRGB>(&leds[startLed], layerP.ledsPerPin[pinIndex]), rgbOrder, options);
         fl::ChannelPtr channel = fl::Channel::create(config);
 
         FastLED.add(channel);
 
-        // FastLED.addLeds<WS2812, 16, GRB>(leds, layerP.ledsPerPin[pinIndex]); this works!!! (but this is static)
+        // FastLED.addLeds<WS2812, 16, GRB>(leds, layerP.ledsPerPin[pinIndex]);  // this works!!! (but this is static)
 
         startLed += layerP.ledsPerPin[pinIndex];
 
         CLEDController* pCur = CLEDController::head();
         EXT_LOGD(ML_TAG, "controller p:%p", pCur);
         while (pCur) {
-          EXT_LOGD(ML_TAG, "controller %d", pCur->size());
+          EXT_LOGD(ML_TAG, "controller %d %d", pCur->size(), pCur->getEnabled());
           pCur = pCur->next();
         }
 
       }  // for pinIndex < nrOfPins
 
       // Check which drivers are available
+      auto drivers = FastLED.getDriverInfos();
       for (size_t i = 0; i < FastLED.getDriverCount(); i++) {
-        auto drivers = FastLED.getDriverInfos();
         auto& info = drivers[i];
         EXT_LOGD(ML_TAG, "Driver: %s, Priority: %d, Enabled: %s", info.name.c_str(), info.priority, info.enabled ? "yes" : "no");
       }
@@ -147,12 +272,19 @@ class FastLEDDriver : public Node {
       CLEDController* pCur = CLEDController::head();
       EXT_LOGD(ML_TAG, "controller p:%p", pCur);
       while (pCur) {
-        EXT_LOGD(ML_TAG, "controller %d", pCur->size());
+        EXT_LOGD(ML_TAG, "controller %d %d", pCur->size(), pCur->getEnabled());
         pCur = pCur->next();
       }
     }
 
     FastLED.setMaxPowerInMilliWatts(1000 * layerP.maxPower);  // 5v, 2000mA, to protect usb while developing
+  }
+
+  ~FastLEDDriver() override {
+    // for (auto channel: channels)
+    {
+      // FastLED.remove(channel);
+    }
   }
 };
 
